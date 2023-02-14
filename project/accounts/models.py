8d6973dd0ae17888ecdd71604733153eb78d17e5
dream <dream_cacao_jh@naver.com>
2023-02-14 23:05:59 +0900

    make login

---
+++ project/accounts/models.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from .managers import UserManager
+
+
+class User(AbstractUser):
+    username = models.CharField(max_length=30, unique=True)
+    email = models.EmailField(_('email address'), unique=True)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.username
+
+
